db8bb2d850637d6264dc33e0f37f4a9130eca4b6
ZavodZeta <zavodzetastana@tutamail.com>
2026-06-29 15:41:42 +0500

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -9,13 +9,13 @@ function initDashboard(products) {
 
     drawKPI(products);
 
-    drawABCChart(products);
-
-    drawXYZChart(products);
+    drawCharts(products);
 
     drawMatrix(products);
 
     drawTable(products);
+
+    console.log("Dashboard готов.");
 
 }
 
@@ -25,9 +25,15 @@ function initDashboard(products) {
 
 function drawKPI(products){
 
-    const revenue = products.reduce((sum,item)=>sum+item.revenue,0);
+    const revenue = products.reduce(
+        (sum,item)=>sum+item.revenue,
+        0
+    );
 
-    const profit = products.reduce((sum,item)=>sum+item.profit,0);
+    const profit = products.reduce(
+        (sum,item)=>sum+item.profit,
+        0
+    );
 
     const sku = products.length;
 
