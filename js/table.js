08325bdff7853e461a2c16ff5ecf1afe171920ff
ZavodZeta <zavodzetastana@tutamail.com>
2026-06-29 16:13:27 +0500

    Update table.js

--- js/table.js
+++ js/table.js
@@ -1,6 +1,5 @@
 // ===========================================
 // table.js
-// Таблица товаров
 // ===========================================
 
 function drawTable(products){
@@ -9,22 +8,27 @@ function drawTable(products){
 
     tbody.innerHTML = "";
 
-    // Сортируем по прибыли
-    products.sort((a,b)=>b.profit-a.profit);
+    products.sort((a,b)=>(b.profit||0)-(a.profit||0));
 
     products.forEach(item=>{
 
-        const tr = document.createElement("tr");
+        const tr=document.createElement("tr");
 
-        tr.innerHTML = `
-            <td>${item.sku}</td>
-            <td>${item.name}</td>
-            <td>${item.revenue.toLocaleString("ru-RU")} ₸</td>
-            <td>${item.profit.toLocaleString("ru-RU")} ₸</td>
-            <td>${item.abc}</td>
-            <td>${item.xyz}</td>
-            <td>${(item.cv*100).toFixed(1)} %</td>
-            <td><strong>${item.matrix}</strong></td>
+        tr.innerHTML=`
+            <td>${item.sku || ""}</td>
+            <td>${item.name || ""}</td>
+
+            <td>${(item.revenue || 0).toLocaleString("ru-RU")} ₸</td>
+
+            <td>${(item.profit || 0).toLocaleString("ru-RU")} ₸</td>
+
+            <td>${item.abc || "-"}</td>
+
+            <td>${item.xyz || "-"}</td>
+
+            <td>${((item.cv || 0)*100).toFixed(1)} %</td>
+
+            <td>${item.matrix || "-"}</td>
         `;
 
         tbody.appendChild(tr);
